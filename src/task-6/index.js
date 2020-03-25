b795f03b6ad5436a98f4491b24b9db22f5950eaf
Maxim Sudakov <grax922@gmail.com>
2020-03-25 22:58:24 +0300

    task-6(2): refactor function

--- src/task-6/index.js
+++ src/task-6/index.js
@@ -38,3 +38,43 @@ function parentheses(str) {
   return countRound === 0 || countSquare === 0 || countCurly === 0 || countAngle === 0;
 }
 console.log(str1, parentheses(str1))
+
+//версия 2.0
+
+function parentheses2(str) {
+  let arr = str.split('');
+  let bracketsLeft = [...arr];
+  let stack = [];
+	let brakets = {
+  	opened: {
+  		round: '(',
+  		square: '[',
+  		curly: '{',
+  		angle: '<',
+  	},
+    closed: {
+    	round: ')',
+  		square: ']',
+  		curly: '}',
+  		angle: '>',
+    },
+  };
+  
+  for( let i = 0 ; i < arr.length; i++) {
+  	if (Object.values(brakets.opened).indexOf(arr[i]) !== -1) {
+    	stack.push(arr[i]);
+      bracketsLeft.shift();
+    }
+    
+    if (Object.values(brakets.closed).indexOf(arr[i]) !== -1) {
+    	let key = Object.keys(brakets.closed).find((k) => brakets.closed[k] === arr[i]);
+    	if (brakets.opened[key] === stack[stack.length-1]) {
+      	stack.pop();
+				bracketsLeft.shift();				      	
+      }
+    }
+  }
+
+  return bracketsLeft.length === 0 && stack.length === 0;	 
+}
+console.log(parentheses2(str1));
